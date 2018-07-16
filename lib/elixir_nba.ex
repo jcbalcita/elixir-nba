fa0669b72afa341f854c08f5652a5a48057ce10d
jbalcita <jbalcita@cj.com>
2018-07-16 15:54:46 -0700

    initial commit

---
+++ lib/elixir_nba.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNba do
+  @moduledoc """
+  Documentation for ElixirNba.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNba.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_nba_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNbaTest do
+  use ExUnit.Case
+  doctest ElixirNba
+
+  test "greets the world" do
+    assert ElixirNba.hello() == :world
+  end
+end
