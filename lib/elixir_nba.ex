e7b40d6cd8856fb9c37bc08fb6283ac211bdeea5
jbalcita <jbalcita@cj.com>
2018-07-18 17:04:04 -0700

    formatting

--- lib/elixir_nba.ex
+++ lib/elixir_nba.ex
@@ -1,15 +1,15 @@
 defmodule ElixirNba do
   alias ElixirNba.Parser
+
   @moduledoc """
    Elixir implementation of bttmly/nba-client-template
   """
 
-  Parser.endpoints
+  Parser.endpoints()
   |> Enum.each(fn endpoint ->
     name = endpoint["name"]
     url = endpoint["url"]
 
     def unquote(:"#{name}")(), do: unquote(url)
   end)
-
 end
