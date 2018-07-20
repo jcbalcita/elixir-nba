712ba1b15661733322b6e7733bb84a8c6906ed91
jbalcita <jbalcita@cj.com>
2018-07-19 17:13:15 -0700

    more extensive parser testing

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -4,8 +4,18 @@ defmodule ParserTest do
   alias ElixirNba.Parser
 
   test "can read parameters and endpoints from json file" do
-    assert Enum.count(Parser.parameters()) == 77
-    assert Enum.count(Parser.endpoints()) == 32
+    # when
+    parameters = Parser.parameters()
+    endpoints = Parser.endpoints()
+
+    # then
+    Enum.each(endpoints, fn p ->
+      assert Map.has_key?(p, "name") && Map.has_key?(p, "url") && Map.has_key?(p, "parameters")
+    end)
+
+    Enum.each(parameters, fn p ->
+      assert Map.has_key?(p, "name") && Map.has_key?(p, "default") && Map.has_key?(p, "values")
+    end)
   end
 
   test "creates headers map from json file" do
@@ -13,10 +23,15 @@ defmodule ParserTest do
     headers = Parser.headers()
 
     # then
+    assert headers["Referer"] == "http://stats.nba.com"
+    assert headers["Referrer"] == "http://stats.nba.com"
+    assert headers["Origin"] == "http://stats.nba.com"
+    assert headers["Accept-Encoding"] == "gzip, deflate"
+    assert headers["Accept-Language"] == "en-US"
+    assert headers["Accept"] == "*/*"
+    assert headers["Connection"] == "keep-alive"
+    assert headers["Cache-Control"] == "no-cache"
     assert headers["User-Agent"] ==
              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0"
-
-    assert headers["Referer"] == "http://stats.nba.com"
-    assert headers["Origin"] == "http://stats.nba.com"
   end
 end
