3daccd1588fe46f349c1c1bf6c66d2937e3e9eac
jbalcita <jbalcita@cj.com>
2018-07-23 12:14:52 -0700

    add necessary hex.pm stuff

--- mix.exs
+++ mix.exs
@@ -7,9 +7,26 @@ defmodule ElixirNba.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
     ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "nba.json"],
+      maintainers: ["John Carlo Aspiras Balcita"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jcbalcita/elixir-nba"}
+    ]
+  end
+
+  defp description do
+    """
+    Elixir client for various stats.nba.com API endpoints
+    """
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/fakes"]
@@ -26,7 +43,8 @@ defmodule ElixirNba.MixProject do
   defp deps do
     [
       {:poison, "~> 3.1"},
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
