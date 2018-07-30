086c7d1293f9957bc857d52a209e6a2fc11532f7
jbalcita <jbalcita@cj.com>
2018-07-30 16:53:17 -0700

    handle single results set

--- lib/parser.ex
+++ lib/parser.ex
@@ -3,7 +3,12 @@ defmodule Nba.Parser do
 
   @spec transform_api_response({atom(), map()}) :: map()
   def transform_api_response({:ok, json}) do
-    json["resultSets"]
+    results_key = case json["resultSets"] do
+      nil -> "resultSet"
+      _ -> "resultSets"
+    end
+
+    json[results_key]
     |> Enum.reduce(%{}, fn result_set, acc ->
       name = result_set["name"]
 
